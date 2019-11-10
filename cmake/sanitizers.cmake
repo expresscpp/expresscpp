9538749dccd2b33844e6d96de36fb69c13b5e7ff
Carlos Gomes Martinho <kmartinho8@gmail.com>
2019-11-10 09:27:14 +0000

    ci: add sanitizers

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -32,3 +32,12 @@ if(EXPRESSCPP_USE_UNDEFINED_SANITIZER)
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined")
   set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fsanitize=undefined ")
 endif(EXPRESSCPP_USE_UNDEFINED_SANITIZER)
+
+if(EXPRESSCPP_USE_THREAD_SANITIZER)
+  set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=thread -g")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=thread -g")
+  set(CMAKE_LINKER_FLAGS_DEBUG "${CMAKE_LINKER_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=thread")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=thread")
+  set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fsanitize=thread ")
+endif(EXPRESSCPP_USE_THREAD_SANITIZER)
+
